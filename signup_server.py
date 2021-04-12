99858d1df0b91e8caac557ff2be9b9d48b4ef558
Kai Zhang <kyle1001001@gmail.com>
2021-04-11 17:28:26 -0700

    prod

--- signup_server.py
+++ signup_server.py
@@ -3,4 +3,4 @@ from signup import app
 # 1. Need to open port 5000 on the server: sudo ufw allow 5000
 # 2. Accept network traffic: host='0.0.0.0'
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True) 
+    app.run(host='0.0.0.0', port=5000, debug=False) 
