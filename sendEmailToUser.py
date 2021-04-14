7a916491a9c97dbd159f6b8da8846f420ae7d43a
Kai Zhang <kyle1001001@gmail.com>
2021-04-14 13:05:24 -0700

    unregister

--- sendEmailToUser.py
+++ sendEmailToUser.py
@@ -22,7 +22,9 @@ def emailToUser(email_list, url):
     to = email_list
     subject = 'COVID Vaccine Appointments are Available!'
     body = '''\n There is an opening slot available for you for the covid vaccine.\n\n Check out '''+ url + '''
-    \n\n Best of wishes for you, thanks for using our covid vaccine app! \n\n- KZhang Apps
+    \n\n Best of wishes for you, thanks for using our covid vaccine app! 
+    \n- KZhang Apps
+    \n\n Unregister at https://cutt.ly/myshot
     '''
     
     message = 'Subject: {}\n\n{}'.format(subject, body)
